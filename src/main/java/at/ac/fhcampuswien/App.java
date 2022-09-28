8ee5484e38f15eb83cbf72c2443d0cc86ee0c14c
thordal <thordal@simetzberger.at>
2022-09-28 19:44:34 +0200

    all tasks complete

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -6,52 +6,159 @@ public class App {
 
     //todo Task 1
     public void sayHelloWorld(){
-        // input your solution here
+        System.out.println("Hello World!");
     }
 
     //todo Task 2
     public void helloRobot(){
-        // input your solution here
+        int counter = 0;
+        for (int i = 0; i < 22; i++) {
+            System.out.print(counter);
+            counter++;
+            if (counter == 10) {
+                counter = 0;
+            }
+        }
+        System.out.println("");
+        String output =
+        "         __" + System.lineSeparator() +
+        " _(\\    |@@|" + System.lineSeparator()   +
+        "(__/\\__ \\--/ __"+ System.lineSeparator() +
+        "   \\___|----|  |   __"+ System.lineSeparator() +
+        "       \\ }{ /\\ )_ / _\\" + System.lineSeparator() +
+        "       /\\__/\\ \\__O (__" + System.lineSeparator() +
+        "      (--/\\--)    \\__/" + System.lineSeparator() +
+        "      _)(  )(_" + System.lineSeparator() +
+        "     `---''---`" + System.lineSeparator();
+        System.out.print(output);
     }
 
     //todo Task 3
     public void sumOfLiterals(){
-        // input your solution here
+        char erstes = 'Z';
+        int zweites = 0xface;
+        int drittes = 012;
+        long viertes = 80L;
+        float fuenftes = 44e-1f;
+        float sechstes = 5.5f;
+        double siebtes = 8.88e1;
+        double achtes = 99.9;
+        int summe = (int)erstes + zweites + drittes + (int) viertes + (int) fuenftes + (int) sechstes + (int) siebtes + (int) achtes;
+        System.out.println(summe);
+
     }
 
     //todo Task 4
     public void addTwoNumbers(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int firstDigit = scanner.nextInt();
+        int secondDigit = scanner.nextInt();
+        int sum = firstDigit + secondDigit;
+        System.out.println(sum);
     }
 
     //todo Task 5
     public void swapTwoNumbers(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int firstDigit = scanner.nextInt();
+        int secondDigit = scanner.nextInt();
+        System.out.println("Before Swap:");
+        System.out.println("x: y: After Swap:");
+        firstDigit = firstDigit ^ secondDigit;
+        secondDigit = firstDigit ^ secondDigit;
+        firstDigit = firstDigit ^ secondDigit;
+        System.out.println("x: " + firstDigit);
+        System.out.println("y: " + secondDigit);
     }
 
     //todo Task 6
     public void compareTwoNumbers(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int firstDigit = scanner.nextInt();
+        int secondDigit = scanner.nextInt();
+        System.out.print("n1: ");
+        System.out.print("n2: ");
+        if (firstDigit == secondDigit) {
+            System.out.println("n1 == n2");
+        } else if (firstDigit > secondDigit) {
+            System.out.println("n1 > n2");
+        }else if (firstDigit < secondDigit) {
+            System.out.println("n2 > n1");
+        }
     }
 
     //todo Task 7
     public void ratingSalesPerson(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter annual Revenue: ");
+        int revenue = scanner.nextInt();
+
+        if (revenue < 0 || revenue >= 100000) {
+            System.out.println("Invalid Revenue");
+        } else if (revenue >= 0 && revenue < 20000) {
+            System.out.println("Poor Sales Revenue");
+        } else if (revenue < 50000 && revenue >= 20000) {
+            System.out.println("Average Sales Revenue");
+        } else if (revenue < 80000 && revenue >= 50000) {
+            System.out.println("Good Sales Revenue");
+        }else if (revenue < 100000 && revenue >= 80000) {
+            System.out.println("Excellent Sales Revenue");
+        }
+
     }
 
     //todo Task 8
     public void getCommissionRate(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter CommissionClass: ");
+        float userinput = scanner.nextFloat();
+
+        if (userinput == 1) {
+            System.out.println("Your Commission Rate was set to 0.01");
+        } else if (userinput == 2) {
+            System.out.println("Your Commission Rate was set to 0.02");
+        }else if (userinput == 3) {
+            System.out.println("Your Commission Rate was set to 0.03");
+        }else if (userinput == 4) {
+            System.out.println("Your Commission Rate was set to 0.04");
+        }else {
+            System.out.println("Your Commission Rate was set to 0.0");
+        }
     }
 
     //todo Task 9
     public void leapyear(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Year: ");
+        int year = scanner.nextInt();
+
+        if (year % 4 != 0) {
+            System.out.println("Not a Leapyear");
+        } else if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                System.out.println("Leapyear");
+            }else {
+                System.out.println("Not a Leapyear");
+            }
+        }else {
+            System.out.println("Not a Leapyear");
+        }
     }
 
     //todo Task 10
     public void transposedNumbers(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int userinput = scanner.nextInt();
+
+        if (userinput < 999 && userinput > 0) {
+            int ones_place = userinput % 10;
+            userinput = userinput / 10;
+            int tens_place = userinput % 10;
+            userinput = userinput / 10;
+            int hundreds_place = userinput;
+            System.out.println(ones_place + "" + tens_place + "" + hundreds_place);
+        }
     }
 
 
